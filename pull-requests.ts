9b48c904fa6879d34479e06ace522fcd3a401950
Prasanna <mail.prasanna.v@gmail.com>
2023-08-10 16:52:49 +0530

    removing the url

--- pull-requests.ts
+++ pull-requests.ts
@@ -2,7 +2,7 @@ import { PullRequest } from "./types";
 import * as pullRequestResponse from './sample.json';
 
 const url =
-  "https://dev.azure.com/BuiltEnvironmentEngineering/_apis/git/repositories/bfd2209b-a8d2-4ec9-a930-96a23519cf28/pullrequests?api-version=6.1-preview.1?searchCriteria.status=completed";
+  "";
 const reviewerId = "fa119f69-8959-4e83-9a48-abba9d6958a1";
 
 function getRaisedPRs(pullRequests: PullRequest[]) {
